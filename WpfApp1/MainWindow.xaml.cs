1db1d1732ab741c2bbf3be8a3580565b0e4c8662
rui-81696 <al81696@alunos.utad.pt>
2025-03-12 10:08:40 +0000

    novo

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -8,7 +8,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-
+dasdasmkldkjsajdkl
 namespace WpfApp1
 {
     /// <summary>
@@ -22,3 +22,5 @@ namespace WpfApp1
         }
     }
 }
+
+daskdasjkdjaskdajklsdjkljklsa
